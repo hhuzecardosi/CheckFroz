de2921e5e1442ade994c49f6ca648ac5827ed54c
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-10-18 21:44:21 +0200

    feat(init): :sparkles: add .env

--- index.ts
+++ index.ts
@@ -2,12 +2,20 @@ import {Elysia} from "elysia";
 import {usersRoutes} from "./src/users/users.controller.ts";
 import swagger from "@elysiajs/swagger";
 
+declare module "bun" {
+  interface Env {
+    PORT: number;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+  }
+}
+
 const app = new Elysia()
 
 app.use(swagger({
   path: '/v1/doc',
   documentation: {
-    info: { title: 'CheckFroz Documentation', version: '1.0.0', description: '' },
+    info: {title: 'CheckFroz Documentation', version: '1.0.0', description: ''},
     tags: [
       {name: 'Users', description: ''}
     ]
@@ -16,6 +24,6 @@ app.use(swagger({
 
 app.use(usersRoutes);
 
-app.listen(3000, () => {
+app.listen(Bun.env.PORT, () => {
   console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
 });
