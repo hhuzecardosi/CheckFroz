a53b12556f88e4c5f6df376a5eeec28bc925327f
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-10-30 21:17:19 +0100

    feat(import): :construction: update test and output before db push

--- test/import.test.ts
+++ test/import.test.ts
@@ -1,11 +1,20 @@
 import {describe, test} from "bun:test";
 import _ from "lodash";
+import {jsonToObject, MappedObject} from "../src/utils/import.ts";
 
 describe('Import Data from Gouv API Orchestration', () => {
   test("Launch all procedure", async () => {
+    const path = 'test/assets-test/response-data.json';
+    const file = Bun.file(path);
 
+    const requestJson = await file.json();
+    const publications = _.get(requestJson, 'Publications.PublicationDetail', []);
+
+    const mappedObject: MappedObject[] = publications.map((publication: Map<string, string | Object>) => jsonToObject(publication));
+
+    const stringMappedObject: string = JSON.stringify(mappedObject);
+
+    const outputFile = Bun.file('test/assets-test/output-data.json');
+    await Bun.write(outputFile, stringMappedObject);
   });
-  test('Test mapping', async () => {
-
-  })
 });
