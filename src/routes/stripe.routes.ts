75485f5907dafbd72cc25f9ac08a470823c1a959
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-11-17 10:19:03 +0100

    feat(rendu): :construction:

--- src/routes/stripe.routes.ts
+++ src/routes/stripe.routes.ts
@@ -1,9 +1,7 @@
 import {Elysia} from "elysia";
 import {handleWebhookResponse} from "../controller/stripe.service.ts";
-import {Buffer} from "buffer";
-import _ from "lodash";
 
 export const stripeRoutes = new Elysia({prefix: '/stripe'})
-.post('/webhook', async ({body, request}) => {
+.post('/webhook', async ({body}) => {
   return await handleWebhookResponse(body);
 }, {detail: {tags: ['stripe']}});
